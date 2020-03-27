34c6b2667fad7550f7915b04f382f499a311cbd6
MaryaKharya <korcemkinamaria76@gmail.com>
2020-03-27 11:55:12 +0300

    изменено

--- lw3/task1/RemoveExtraBlanks.php
+++ lw3/task1/RemoveExtraBlanks.php
@@ -1,12 +1,15 @@
 <?php
 
 header("Content-Type: text/plain");
-$name = $_GET['text'];
-$name = trim($name, ' ');
-$length = strlen($name);
-for ($i = 0; $i < $length; $i++)
+if ($_GET['text'])
 {
-    $name = str_replace("  ", " ", $name);
+    $name = $_GET['text'];
+    $name = trim($name, ' ');
+    $length = strlen($name);
+    for ($i = 0; $i < $length; $i++)
+    {
+        $name = str_replace("  ", " ", $name);
+    }
+    echo $name;
 }
-echo $name;
      
