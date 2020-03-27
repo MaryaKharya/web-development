2da7e215505b78f7bc3e51efd104e6fdf17afc69
MaryaKharya <korcemkinamaria76@gmail.com>
2020-03-27 11:59:02 +0300

    изменено

--- lw3/task3/PasswordStrength.php
+++ lw3/task3/PasswordStrength.php
@@ -1,46 +1,49 @@
 <?php
 
-$password = $_GET['password'];
-$strength = 0;
-$len = strlen($password);
-if (preg_match("/^[aA-zZ0-9]+$/", $password))
+if ($_GET['password'])
 {
-    $n = $len;
-    $strength = 4*$n;
-    if (preg_match_all( '/[0-9]/', $password, $matches))
+    $password = $_GET['password'];
+    $strength = 0;
+    $len = strlen($password);
+    if (preg_match("/^[aA-zZ0-9]+$/", $password))
     {
-        $n = count($matches[0]);
-        $strength = $strength+$n*4;
-    }
-    if (preg_match_all( '/[A-Z]/', $password, $matches))
-    {
-        $n = count($matches[0]);
-        $strength = $strength+(($len-$n)*2);
-    }
-    if (preg_match_all( '/[a-z]/', $password, $matches))
-    {
-        $n = count($matches[0]);
-        $strength = $strength+(($len-$n)*2);
-    }
-    if (preg_match("/^[aA-zZ]+$/", $password))
-    {
-        $strength = $strength-$len;
-    }
-    if (preg_match("/^[0-9]+$/", $password))
-    {
-        $strength = $strength-$len;
-    }
-    foreach (count_chars($password, 1) as $i => $val) 
-    {
-        $n = $val;
-        if ($n > 1)
+        $n = $len;
+        $strength = 4*$n;
+        if (preg_match_all( '/[0-9]/', $password, $matches))
         {
-            $strength = $strength-$n;
+            $n = count($matches[0]);
+            $strength = $strength+$n*4;
         }
+        if (preg_match_all( '/[A-Z]/', $password, $matches))
+        {  
+            $n = count($matches[0]);
+            $strength = $strength+(($len-$n)*2);
+        }
+        if (preg_match_all( '/[a-z]/', $password, $matches))
+        {
+            $n = count($matches[0]);
+            $strength = $strength+(($len-$n) * 2);
+        }
+        if (preg_match("/^[aA-zZ]+$/", $password))
+        {
+            $strength = $strength-$len;
+        }
+        if (preg_match("/^[0-9]+$/", $password))
+        {
+            $strength = $strength-$len;
+        }
+        foreach (count_chars($password, 1) as $i => $val) 
+        {
+            $n = $val;
+            if ($n > 1)
+            {
+                $strength = $strength-$n;
+            }
+        }
+        echo $strength;
     }
-    echo $strength;
-}
-else
-{
-     echo $strength;
-}    
+    else
+    {
+        echo $strength;
+    } 
+}   
