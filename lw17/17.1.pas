78545100eba5e1599a8ca8ede3747003370636a3
MaryaKharya <61596053+MaryaKharya@users.noreply.github.com>
2020-03-31 13:49:47 +0300

    Update 17.1.pas
    Исправлены ошибки

--- lw17/17.1.pas
+++ lw17/17.1.pas
@@ -1,41 +1,43 @@
 PROGRAM Sum(INPUT, OUTPUT); 
 VAR
-  Number, Digit: INTEGER;
+  Sum, Digit: INTEGER;
 PROCEDURE ReadDigit(VAR InF: TEXT; VAR Digit: INTEGER);
 VAR
   Ch: CHAR;
 BEGIN
-  READ(InF, Ch);
-  IF (Ch = '0') THEN Digit := 0 ELSE
-  IF (Ch = '1') THEN Digit := 1 ELSE
-  IF (Ch = '2') THEN Digit := 2 ELSE
-  IF (Ch = '3') THEN Digit := 3 ELSE
-  IF (Ch = '4') THEN Digit := 4 ELSE
-  IF (Ch = '5') THEN Digit := 5 ELSE
-  IF (Ch = '6') THEN Digit := 6 ELSE
-  IF (Ch = '7') THEN Digit := 7 ELSE
-  IF (Ch = '8') THEN Digit := 8 ELSE
-  IF (Ch = '9') THEN Digit := 9 ELSE
-  Digit := -1     
-END;
-BEGIN
-  IF NOT EOLN
+  Digit := 0;
+  IF NOT EOLN(InF)
   THEN
     BEGIN
-      ReadDigit(INPUT, Number);
-      IF Number <> -1
-      THEN
-        BEGIN 
-          WHILE NOT EOLN(INPUT) AND (Digit <> -1)
-          DO
-            BEGIN
-              ReadDigit(INPUT, Digit);
-              IF (Digit <> -1)
-              THEN
-                Number := Number + Digit 
-            END;
-          WRITE(OUTPUT, Number)
+      READ(InF, Ch);
+      IF (Ch = '0') THEN Digit := 0 ELSE
+      IF (Ch = '1') THEN Digit := 1 ELSE
+      IF (Ch = '2') THEN Digit := 2 ELSE
+      IF (Ch = '3') THEN Digit := 3 ELSE
+      IF (Ch = '4') THEN Digit := 4 ELSE
+      IF (Ch = '5') THEN Digit := 5 ELSE
+      IF (Ch = '6') THEN Digit := 6 ELSE
+      IF (Ch = '7') THEN Digit := 7 ELSE
+      IF (Ch = '8') THEN Digit := 8 ELSE
+      IF (Ch = '9') THEN Digit := 9 ELSE
+      Digit := -1
+    END  
+END;
+BEGIN
+  ReadDigit(INPUT, Digit);
+  IF Digit <> -1
+  THEN
+    BEGIN
+      Sum := Digit; 
+      WHILE NOT EOLN(INPUT) AND (Digit <> -1)
+      DO
+        BEGIN
+          ReadDigit(INPUT, Digit);
+          IF (Digit <> -1)
+          THEN
+            Sum := Sum + Digit 
         END;
-      WRITELN
-    END
+      WRITE(OUTPUT, Sum)
+    END;
+  WRITELN
 END.    
