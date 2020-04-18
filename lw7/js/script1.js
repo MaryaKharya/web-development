16d23f8c87c255c585d56dfd9864561f994614b1
MaryaKharya <61596053+MaryaKharya@users.noreply.github.com>
2020-04-18 19:29:31 +0300

    Update script1.js

--- lw7/js/script1.js
+++ lw7/js/script1.js
@@ -2,34 +2,34 @@ function isPrimeNumber(n) {
   if (Array.isArray(n)) {
     for (let i of n) {
       if (typeof i === 'number') {
-        if (PrimeElement(i)) {
-          console.log(`${i} is prime number`)
+        if (primeElement(i)) {
+          console.log(`${i} is prime number`);
         } else {
-            console.log(`${i} is not prime number`)
+            console.log(`${i} is not prime number`);
           }
       } else {
-          console.log('data entered incorrectly') 
+          console.log('data entered incorrectly'); 
         }
     }
   } else {
       if (typeof n === 'number') {
         if (primeElement(n)) {
-          console.log(`${n} is prime number`)
+          console.log(`${n} is prime number`);
         } else {
-            console.log(`${n} is not prime number`)
+            console.log(`${n} is not prime number`);
           }
       } else {
-          console.log('data entered incorrectly')
+          console.log('data entered incorrectly');
         }
     }
 }
 
 function primeElement(e) {
-    let isPrime = true
+    let isPrime = true;
     for (let j = 2; j < e; j++) {
       if (e % j == 0) {
-        isPrime = false
+        isPrime = false;
       }
     }
-    return isPrime
+    return isPrime;
 }
