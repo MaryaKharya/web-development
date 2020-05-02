31794888b3e96de08e0aa8e8b536f5860c53d357
MaryaKharya <61596053+MaryaKharya@users.noreply.github.com>
2020-05-02 21:11:45 +0300

    Update PasswordStrength.php

--- lw3/task3/PasswordStrength.php
+++ lw3/task3/PasswordStrength.php
@@ -5,32 +5,32 @@ if ($_GET['password'])
     $password = $_GET['password'];
     $strength = 0;
     $len = strlen($password);
+    $n = strlen($password);
     if (preg_match("/^[aA-zZ0-9]+$/", $password))
     {
-        $n = $len;
         $strength = 4*$n;
         if (preg_match_all( '/[0-9]/', $password, $matches))
         {
             $n = count($matches[0]);
-            $strength = $strength+$n*4;
+            $strength += $n*4;
         }
         if (preg_match_all( '/[A-Z]/', $password, $matches))
         {  
             $n = count($matches[0]);
-            $strength = $strength+(($len-$n)*2);
+            $strength += ($len-$n)*2;
         }
         if (preg_match_all( '/[a-z]/', $password, $matches))
         {
             $n = count($matches[0]);
-            $strength = $strength+(($len-$n) * 2);
+            $strength += ($len-$n) * 2;
         }
         if (preg_match("/^[aA-zZ]+$/", $password))
         {
-            $strength = $strength-$len;
+            $strength -= $len;
         }
         if (preg_match("/^[0-9]+$/", $password))
         {
-            $strength = $strength-$len;
+            $strength -= $len;
         }
         foreach (count_chars($password, 1) as $i => $val) 
         {
