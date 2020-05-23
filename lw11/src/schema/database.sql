5e6d5a7dd8fdedbab9ec623bfbf89ddb576f36d2
MaryaKharya <61596053+MaryaKharya@users.noreply.github.com>
2020-05-23 09:41:31 +0300

    Update database.sql

--- lw11/src/schema/database.sql
+++ lw11/src/schema/database.sql
@@ -5,7 +5,7 @@ USE lw11;
 CREATE TABLE user
 (
     id              INT AUTO_INCREMENT NOT NULL,
-    name      VARCHAR(255)       NOT NULL,
+    name            VARCHAR(255)       NOT NULL,
     email           VARCHAR(255)       NOT NULL,
     country         VARCHAR(255)       NOT NULL,
     gender          VARCHAR(255)       NOT NULL,
